bef8d61f19f26ce9aec6c3a86d33fc56ce76c73d
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-01-22 12:38:03 +0100

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; // Define loginForm como un FormGroup
 
+  constructor() {}
+
+  ngOnInit() {
+    // Inicializa el formulario y sus controles
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+
+      // Agrega aquí la lógica para enviar la solicitud de inicio de sesión con username y password
+    }
+  }
 }
