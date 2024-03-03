6a301fb3507515571c77bf5352482c8498358ae9
Juanite <juan13_barsa@hotmail.com>
2024-03-03 06:16:17 +0100

    manager ticket creation and supportTechnician dashboard done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,20 +9,21 @@ import { TicketsComponent } from './pages/tickets/tickets.component';
 import { HelpComponent } from './pages/help/help.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SupportManagerComponent } from './pages/support-manager/support-manager.component';
+import { ManagerNewComponent } from './pages/manager-new/manager-new.component';
 
 
 const routes: Routes = [
   {path: 'homepage', component: HomepageComponent, canActivate: []},
   {path: 'login', component: LoginComponent, canActivate: []},
   {path: 'support-manager', component: SupportManagerComponent, canActivate: []},
-  {path: 'support-technicial', component: SupportTechnicialComponent, canActivate: []},
+  {path: 'support-technician', component: SupportTechnicialComponent, canActivate: []},
   {path: '', redirectTo: 'new'/*'homepage'*/, pathMatch: 'full'},
   {path: "new", component: NewComponent, canActivate: []},
   {path: "tickets", component: TicketsComponent, canActivate: []},
   {path: "help", component: HelpComponent, canActivate: []}, 
   {path: 'dashboard', component: DashboardComponent, canActivate:[]},
+  {path: 'manager-new', component: ManagerNewComponent, canActivate: []},
   {path: '**', component: Error404Component},
- 
 
 ];
 
